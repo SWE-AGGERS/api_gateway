4655b0751abdfefb5951c23d67ff4a08ba35c67f
Khaos <danimorpg@gmail.com>
2019-11-25 10:18:57 +0100

    Updated to the new Microservice system. NB: need the /story_list/user/limit API to work correctly

--- views/users.py
+++ views/users.py
@@ -9,35 +9,21 @@ from api_gateway.views.stories import reacted
 
 users = Blueprint('users', __name__)
 
-
-# Reducing the join (User x Story on user.id = story.author_id) 
-# to a list of <User, Latest User's Story>
-def reduce_list(users_):
-    users = []
-    aux = []
-    for story, user in users_:
-        if user.id not in aux:
-            users.append(
-                (story, user)
-            )
-            aux.append(user.id)
-
-    return users
-
-
 @users.route('/users')
 def _users():
-    users = db.session.query(Story, User).join(Story).order_by(desc(Story.id))
-    users = reduce_list(users.all())
-
+    users = get_users_s()
+    user_stories = []
+    for user in users:
+        user_stories.append((get_stories_s(user["id"], limit=1)[0], user))
+    
     users = list(
         map(lambda x: (
             x[0],
             x[1],
-            "hidden" if x[1].id == current_user.id else "",
-            "unfollow" if _is_follower(current_user.id, x[1].id) else "follow",
-            reacted(current_user.id, x[0].id)
-        ), users)
+            "hidden" if x[1]["id"] == current_user.id else "",
+            "unfollow" if _is_follower(current_user.id, x[1]["id"]) else "follow",
+            reacted(current_user.id, x[0]["id"])
+        ), user_stories)
     )
     
     return render_template(
@@ -46,3 +32,22 @@ def _users():
         like_it_url="/stories/reaction",
         details_url="/stories"
     )
+
+
+def get_stories_s(userid, limit=0):
+    # call story service
+    if limit > 0:
+        reply = request.get('http://' + STORIES_SERVICE_IP + ':' + STORIES_SERVICE_PORT + '/story_list/'+str(userid)+'/'+str(limit), timeout=1)
+    else:
+        reply = request.get('http://' + STORIES_SERVICE_IP + ':' + STORIES_SERVICE_PORT + '/story_list/'+str(userid), timeout=1)
+    body = json.loads(str(reply.data, 'utf8'))
+    # Gestire errori in base al campo result di body
+    return body['stories']
+
+
+def get_users_s():
+    """Get the list of all users"""
+    url = 'http://' + USERS_SERVICE_IP + ':' + USERS_SERVICE_PORT + '/users'
+    # TODO error
+    reply = request.get(url, timeout=1)
+    return json.loads(str(reply.data, 'utf8'))
