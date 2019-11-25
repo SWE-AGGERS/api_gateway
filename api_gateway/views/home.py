ef70ad3f659152a13e1cd4b7e3a0d07d9ec1734d
Khaos <danimorpg@gmail.com>
2019-11-25 17:28:39 +0100

    home.py updated to Microservice system

--- api_gateway/views/home.py
+++ api_gateway/views/home.py
@@ -3,6 +3,9 @@ from flask import Blueprint, render_template
 from api_gateway.auth import current_user
 from api_gateway.database import db, Story
 
+from api_gateway.constants import STORIES_SERVICE_IP, STORIES_SERVICE_PORT
+
+
 home = Blueprint('home', __name__)
 
 
@@ -13,7 +16,19 @@ def _strava_auth_url(config):
 @home.route('/')
 def index():
     if current_user is not None and hasattr(current_user, 'id'):
-        stories = db.session.query(Story).filter(Story.author_id == current_user.id)
+        stories = dget_story_by_author_id(current_user.id)
     else:
         stories = None
     return render_template("index.html", stories=stories, active_button="index")
+
+def get_story_by_author_id(author_id):
+    url = 'http://' + STORIES_SERVICE_IP + ':' + STORIES_SERVICE_PORT + '/story_list/' + str(author_id)
+    try:
+	    reply = request.get(url, timeout=1)
+	    story =  json.loads(str(reply.data))
+	    if story["result"] == 1:
+	        return story["story"]
+	    else:
+	        return None
+	except:
+		return None
