3b3e9a691cd8520ec85b9a0fa1f514fc8e3e29f1
Mauro <mauroorru3@gmail.com>
2019-11-26 20:00:42 +0100

    update_stories

--- service/views/home.py
+++ service/views/home.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
-
 from service.auth import current_user
+import requests
 
 from service.constants import STORIES_SERVICE_IP, STORIES_SERVICE_PORT
 
@@ -15,19 +15,19 @@ def _strava_auth_url(config):
 @home.route('/')
 def index():
     if current_user is not None and hasattr(current_user, 'id'):
-        stories = dget_story_by_author_id(current_user.id)
+        stories = get_story_by_author_id(current_user.id)
     else:
         stories = None
     return render_template("index.html", stories=stories, active_button="index")
 
+
+
 def get_story_by_author_id(author_id):
     url = 'http://' + STORIES_SERVICE_IP + ':' + STORIES_SERVICE_PORT + '/story_list/' + str(author_id)
-    try:
-        reply = request.get(url, timeout=1)
-        story =  json.loads(str(reply.data))
-        if story["result"] == 1:
-            return story["story"]
-        else:
-            return None
-    except:
-        return None
+
+    reply = requests.get(url, timeout=1)
+    json_data = reply.json()
+    if json_data["result"] == 1:
+        return json_data["stories"]
+    else:
+        return []
